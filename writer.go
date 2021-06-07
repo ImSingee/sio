33a6e4bb8e441513266474104004ea11c60d750b
Singee <git@singee.me>
2021-06-07 14:33:58 +0800

    add write counter N & WriteVarUInt, WriteVarInt

--- writer.go
+++ writer.go
@@ -8,6 +8,7 @@ import (
 
 type Writer struct {
 	writer io.Writer
+	n      uint64
 }
 
 func NewWriter(wt io.Writer) *Writer {
@@ -17,26 +18,42 @@ func NewWriter(wt io.Writer) *Writer {
 		return b
 	}
 
-	return &Writer{wt}
+	return &Writer{writer: wt}
+}
+
+// N 返回内部写计数器
+func (w *Writer) N() uint64 {
+	return w.n
+}
+
+// ResetN 将内部写计数器清零
+func (w *Writer) ResetN() {
+	w.n = 0
 }
 
 func (w *Writer) Write(p []byte) (n int, err error) {
-	return w.writer.Write(p)
+	n, err = w.writer.Write(p)
+	w.n += uint64(n)
+	return
 }
 
 func (w *Writer) WriteByte(b byte) error {
-	_, err := w.writer.Write([]byte{b})
+	n, err := w.writer.Write([]byte{b})
+	w.n += uint64(n)
 
 	return err
 }
 
-// 写入全部字符串，如果无法全部写入依然会返回错误
-func (w *Writer) WriteString(s string) (int, error) {
-	return w.writer.Write(*(*[]byte)(unsafe.Pointer(&s)))
+// WriteString 写入全部字符串，如果无法全部写入依然会返回错误
+func (w *Writer) WriteString(s string) (n int, err error) {
+	n, err = w.writer.Write(*(*[]byte)(unsafe.Pointer(&s)))
+	w.n += uint64(n)
+
+	return
 }
 
 func (w *Writer) WriteUInt8(b uint8) (int, error) {
-	return w.writer.Write([]byte{b})
+	return w.Write([]byte{b})
 }
 
 func (w *Writer) WriteUInt16(x uint16) (int, error) {
@@ -66,119 +83,14 @@ func (w *Writer) WriteZeros(n int) (int, error) {
 	return w.Write(bs)
 }
 
-//
-//// 注意：n 的数量不能大于 buffer size
-//func (r *Reader) Peek(n int) ([]byte, error) {
-//	return r.breader.Peek(n)
-//}
-//
-//// 尽可能多的跳过 n 个字节
-//func (r *Reader) Skip(n int) (int, error) {
-//	discard, err := r.breader.Discard(n)
-//
-//	if err == nil { // 跳过足够
-//		return discard, err
-//	}
-//
-//	if err == io.EOF && discard != n { // 未跳过足够
-//		return discard, nil
-//	}
-//
-//	// 其他异常
-//	return discard, err
-//}
-//
-//func (r *Reader) MustSkip(n int) (int, error) {
-//	return r.breader.Discard(n)
-//}
-//
-//func (r *Reader) ReadByte() (byte, error) {
-//	return r.breader.ReadByte()
-//}
-//
-//func (r *Reader) ReadBytes(n int) ([]byte, error) {
-//	if n < 0 {
-//		return nil, fmt.Errorf("cannot read negative bytes")
-//	}
-//	if n == 0 {
-//		return nil, nil
-//	}
-//
-//	result := make([]byte, n)
-//
-//	read, err := r.Read(result)
-//	if err != nil {
-//		return result[:read], err
-//	}
-//
-//	return result[:read], nil
-//}
-//
-//// 读取足够的字节，如果不够会报错
-//// 当 n <= buffer size 时，该函数保证如果读不够则回退
-//// 否则，会停留在最后的读取地
-//func (r *Reader) ReadEnoughBytes(n int) ([]byte, error) {
-//	// 如果 buffer 足够，利用 peek 完成完美回退
-//	bytes, err := r.breader.Peek(n)
-//	if err != bufio.ErrBufferFull {
-//		if err != nil {
-//			return nil, err
-//		}
-//		if len(bytes) != n {
-//			return bytes, fmt.Errorf("no enough bytes can be read")
-//		}
-//
-//		return bytes, nil
-//	}
-//
-//	// 否则正常读取，不考虑回退（TODO 优化）
-//	bytes = make([]byte, n)
-//	read, err := io.ReadFull(r, bytes)
-//
-//	// ReadFull 保证读不够会返回错误，因此可以不额外判断
-//	return bytes[:read], err
-//}
-//
-//func (r *Reader) ReadBytesAsString(n int) (string, error) {
-//	bytes, err := r.ReadEnoughBytes(n)
-//
-//	if err != nil {
-//		return "", err
-//	}
-//
-//	return *((*string)(unsafe.Pointer(&bytes))), nil
-//}
-//
-//func (r *Reader) ReadUInt8() (uint8, error) {
-//	return r.ReadByte()
-//}
-//
-//func (r *Reader) ReadUInt16() (uint16, error) {
-//	bytes, err := r.ReadEnoughBytes(2)
-//
-//	if err != nil {
-//		return 0, err
-//	}
-//
-//	return binary.BigEndian.Uint16(bytes), err
-//}
-//
-//func (r *Reader) ReadUInt32() (uint32, error) {
-//	bytes, err := r.ReadEnoughBytes(4)
-//
-//	if err != nil {
-//		return 0, err
-//	}
-//
-//	return binary.BigEndian.Uint32(bytes), err
-//}
-//
-//func (r *Reader) ReadUInt64() (uint64, error) {
-//	bytes, err := r.ReadEnoughBytes(8)
-//
-//	if err != nil {
-//		return 0, err
-//	}
-//
-//	return binary.BigEndian.Uint64(bytes), err
-//}
+func (w *Writer) WriteVarUInt(x uint64) (int, error) {
+	buf := make([]byte, 16) // 10 byte is enough
+	n := binary.PutUvarint(buf, x)
+	return w.Write(buf[:n])
+}
+
+func (w *Writer) WriteVarInt(x int64) (int, error) {
+	buf := make([]byte, 16) // 10 byte is enough
+	n := binary.PutVarint(buf, x)
+	return w.Write(buf[:n])
+}
